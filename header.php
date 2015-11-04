bd9c225ccc95e079d16fbed76294f7a241a730c3
Arthur Gareginyan <arthurgareginyan@gmail.com>
2015-11-05 01:35:24 +0300

    Fixed typo in "<!--[endif]-->"

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
+<!--[endif]-->
 <!--[if IE 8]>
 <html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
